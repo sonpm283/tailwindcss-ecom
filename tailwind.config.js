8cd442785651f3af09c25061f2208ff01ce234d2
PhanMinhSon <sonpm@hivelab.com>
2024-10-15 00:58:17 +0700

    [tailwind-css] feat: settings

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  corePlugins: {
+    container: false,
+  },
+  theme: {
+    extend: {
+      maxWidth: {
+        1480: "1480px",
+      },
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", "Arial", "sans-serif"],
+      },
+      colors: {
+        primary: "#fa2828",
+        seconday: "#385472",
+      },
+      theme: {
+        screens: {
+          // sm: "640px",
+          // md: "768px",
+          // lg: "1024px",
+          // xl: "1280px",
+          // "2xl": "1536px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
