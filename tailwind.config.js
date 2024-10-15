fd26c20eadc7699ef9bae1d657c099ef5b006dcb
PhanMinhSon <sonpm@hivelab.com>
2024-10-16 01:48:11 +0700

    [tailwind-css] update: home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,6 @@ module.exports = {
   },
   theme: {
     extend: {
-      maxWidth: {
-        1480: "1480px",
-      },
       fontFamily: {
         jakarta: ["Plus Jakarta Sans", "Arial", "sans-serif"],
       },
@@ -16,15 +13,49 @@ module.exports = {
         primary: "#fa2828",
         seconday: "#385472",
         gray: "#ebebe9",
+        lightGray: "#8a8a8a",
       },
-      theme: {
-        screens: {
-          // sm: "640px",
-          // md: "768px",
-          // lg: "1024px",
-          // xl: "1280px",
-          // "2xl": "1536px",
+      spacing: {
+        // 100: "100px",
+      },
+      fontSize: {
+        15: "15px",
+      },
+      maxWidth: {
+        1480: "1480px",
+      },
+      size: {
+        13: "13px",
+      },
+      width: {
+        // 13: "13px",
+      },
+      height: {
+        // 13: "13px",
+      },
+      screens: {
+        // sm: "640px",
+        // md: "768px",
+        // lg: "1024px",
+        // xl: "1280px",
+        // "2xl": "1536px",
+      },
+
+      keyframes: {
+        zoomIn: {
+          "0%": {
+            transform: "scale(1.2)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "scale(1)",
+            opacity: "1",
+          },
         },
+      },
+
+      animation: {
+        zoomIn: "zoomIn .8s ease-out",
       },
     },
   },
